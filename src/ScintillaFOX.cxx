d4a0ee4010da057ea645079dbbb34cb963dbd62f
Gilles Filippini <gilles.filippini@free.fr>
2003-10-11 14:35:20 +0000

    To much Win32 accent testing.

--- src/ScintillaFOX.cxx
+++ src/ScintillaFOX.cxx
@@ -855,8 +855,6 @@ long FXScintilla::onKeyPress(FXObject* sender,FXSelector sel,void* ptr)
 		return 1;
 	int key = ((unsigned int(event->text[0]) >= 32) && !ctrl && !alt) ?
 		event->text[0] : event->code;
-	if ((!event->text[0]) && (event->code >= 32) && (event->code < 256))
-		return 1;
 #endif
 	// </FIXME>
 	if (ctrl && (key < 128))
