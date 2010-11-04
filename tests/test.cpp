b2e4d1758c2d6155ef5e7a8fe1edd7953013d657
Jeff Pohlmeyer <yetanothergeek@gmail.com>
2010-11-04 00:04:34 -0500

    Changed example to use C syntax instead of Ruby

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,54 +4,30 @@
 #include <FXScintilla.h>
 
 // Initial text
-static const char sInitialText[] = "\
-#! /usr/bin/env ruby\n\
+static const char initialText[] = "\
+/* A simple 'hello world' program. */\n\
 \n\
-require \"fox\"\n\
-require \"fox/responder\"\n\
-require \"MainWindow\"\n\
+#include <stdio.h>\n\
 \n\
-include Fox\n\
-\n\
-def main\n\
-  # Make application\n\
-  application = FXApp.new(\"rbCVS\", \"pini\")\n\
-\n\
-  # Open display\n\
-  application.init(ARGV)\n\
-\n\
-  # Set normal font regular\n\
-  font = application.normalFont\n\
-  fd = font.fontDesc\n\
-  fd.weight = FONTWEIGHT_REGULAR\n\
-  font.fontDesc = fd\n\
-\n\
-  # Make window\n\
-  MainWindow.new(application)\n\
-\n\
-  # Create app\n\
-  application.create()\n\
-  # Run\n\
-  application.run()\n\
-end\n\
-\n\
-main\n\
+int main(int argc, char *argv[]) {\n\
+  printf(\"hello world\\n\");\n\
+  return 0;\n\
+}\n\
 ";
 
 // The lexer needs a keywords list
-static const char sRubyKeyWords[] =
-	"__FILE__ and def end in or self unless __LINE__ "
-	"begin defined? ensure module redo super until BEGIN "
-	"break do false next rescue then when END case "
-	"else for nil retry true while alias class elsif "
-	"if not return undef yield";
+static const char cppKeyWords[] =
+  "asm auto break case char const continue default do double "
+  "else enum extern float for goto if inline int long register "
+  "return short signed sizeof static struct switch typedef "
+  "union unsigned void volatile while";
 
-class Test : public FXMainWindow
+
+class Test: public FXMainWindow
 {
 	FXDECLARE(Test)
 protected:
 	FXScintilla * scint;
-protected:
 	Test() {}
 public:
 	enum {
@@ -59,89 +35,96 @@ public:
 		ID_FONT,
 		ID_LAST
 	};
-public:
-	Test(FXApp * app) : FXMainWindow(app, "Test", NULL, NULL, DECOR_ALL, 0, 0, 600, 400)
-	{
-		// Font selector button
-		new FXButton(this, "Change Font", NULL, this, ID_FONT);
-		
-		// FXScintilla widget
-		scint = new FXScintilla(this, this, ID_SCINTILLA, LAYOUT_FILL_X|LAYOUT_FILL_Y);	
-		// Preparing the widget to do syntax coloring for Ruby
-		scint->sendMessage(SCI_SETLEXERLANGUAGE, 0, reinterpret_cast<long>("ruby"));
-		scint->sendMessage(SCI_SETKEYWORDS, 0, reinterpret_cast<long>(sRubyKeyWords));
-		setFont("courier", 12);
-	}
-	virtual void create()
-	{
-		FXMainWindow::create();
-		// Feeding the widget with some initial text
-/*		FXFile file("/home/pini/UTF-8-demo.txt");
-		char buffer[256];
-		FXival read;
-		unsigned int pos = 0;
-		do {
-			read = file.readBlock(buffer, 255);
-			buffer[read] = '\0';
-			scint->sendMessage(SCI_INSERTTEXT, pos, reinterpret_cast<long>(buffer));
-			pos += read;
-		} while (read == 255);
-		scint->setFocus();
-		file.close();*/
-		scint->sendMessage(SCI_INSERTTEXT, 0, reinterpret_cast<long>(sInitialText));
-	}
-	void setFont(const FXString & font, int size)
-	{
-		scint->sendMessage(SCI_STYLESETFONT, STYLE_DEFAULT, reinterpret_cast<long>(font.text()));
-		scint->sendMessage(SCI_STYLESETSIZE, STYLE_DEFAULT, size);
-		scint->sendMessage(SCI_STYLECLEARALL, 0, 0);
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_DEFAULT, FXRGB(0x80, 0x80, 0x80));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_COMMENTLINE, FXRGB(0x00, 0x7f, 0x00));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_NUMBER, FXRGB(0x00, 0x7f, 0x7f));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_STRING, FXRGB(0x7f, 0x00, 0x7f));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_CHARACTER, FXRGB(0x7f, 0x00, 0x7f));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_WORD, FXRGB(0x00, 0x00, 0x7f));
-		scint->sendMessage(SCI_STYLESETBOLD, SCE_P_WORD, 1);
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_TRIPLE, FXRGB(0x7f, 0x00, 0x00));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_TRIPLEDOUBLE, FXRGB(0x7f, 0x00, 0x00));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_CLASSNAME, FXRGB(0x00, 0x00, 0xff));
-		scint->sendMessage(SCI_STYLESETBOLD, SCE_P_CLASSNAME, 1);
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_DEFNAME, FXRGB(0x00, 0x7f, 0x7f));
-		scint->sendMessage(SCI_STYLESETBOLD, SCE_P_DEFNAME, 1);
-		scint->sendMessage(SCI_STYLESETBOLD, SCE_P_OPERATOR, 1);
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_IDENTIFIER, FXRGB(0x7f, 0x7f, 0x7f));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_COMMENTBLOCK, FXRGB(0x7f, 0x7f, 0x7f));
-		scint->sendMessage(SCI_STYLESETFORE, SCE_P_STRINGEOL, FXRGB(0x00, 0x00, 0x00));
-		scint->sendMessage(SCI_STYLESETBACK, SCE_P_STRINGEOL, FXRGB(0xe0, 0xc0, 0xe0));
-		scint->sendMessage(SCI_STYLESETEOLFILLED, SCE_P_STRINGEOL, 1);
-		scint->sendMessage(SCI_STYLESETFORE, 34, FXRGB(0x00, 0x00, 0xff));
-		scint->sendMessage(SCI_STYLESETBOLD, 34, 1);
-		scint->sendMessage(SCI_STYLESETFORE, 35, FXRGB(0xff, 0x00, 0x00));
-		scint->sendMessage(SCI_STYLESETBOLD, 35, 1);
-	}
-	long onCmdFont(FXObject *, FXSelector, void *)
-	{
-		FXFontDialog diag(this, "Toto", 0);
-		if (diag.execute()) {
-#ifdef FOX_1_6
-			FXFontDesc desc;
-			diag.getFontSelection(desc);
-#else
-			FXFontDesc desc=diag.getFontDesc();
-#endif
-			setFont(desc.face, desc.size / 10);
-		}
-		return 1;
-	}
-	long onCmdScintilla(FXObject *, FXSelector, void * ptr)
-	{
-		SCNotification * scn = static_cast<SCNotification *>(ptr);
-		
-		// Add here stuff to handle Scintilla events
-		
-		return 1;
-	}
+  Test(FXApp *app);
+	long onCmdScintilla(FXObject *obj, FXSelector sel, void *ptr);
+	virtual void create();
+	void setFont(const FXString &font, int size);
+	long onCmdFont(FXObject *obj, FXSelector sel, void *ptr);
 };
+
+
+FXColor rgb(const char *rrggbb) {
+  unsigned int r,g,b;
+  sscanf((char*)(rrggbb+1), "%2x%2x%2x", &r,&g, &b);
+  return FXRGB(r,g,b);
+}
+
+
+Test::Test(FXApp *app): FXMainWindow(app, "Test", NULL, NULL, DECOR_ALL, 0, 0, 600, 400)
+{
+  // Font selector button
+  new FXButton(this, " Change Font ", NULL, this, ID_FONT);
+
+  // FXScintilla widget
+  scint = new FXScintilla(this, this, ID_SCINTILLA, LAYOUT_FILL_X|LAYOUT_FILL_Y);
+  // Preparing the widget to do syntax coloring
+  scint->sendMessage(SCI_SETLEXERLANGUAGE, 0, reinterpret_cast<long>("cpp"));
+  scint->sendMessage(SCI_SETKEYWORDS, 0, reinterpret_cast<long>(cppKeyWords));
+  setFont("courier", 12);
+}
+
+
+void Test::create()
+{
+  FXMainWindow::create();
+  scint->sendMessage(SCI_INSERTTEXT, 0, reinterpret_cast<long>(initialText));
+}
+
+
+void Test::setFont(const FXString &font, int size)
+{
+  scint->sendMessage(SCI_STYLESETFONT, STYLE_DEFAULT, reinterpret_cast<long>(font.text()));
+  scint->sendMessage(SCI_STYLESETSIZE, STYLE_DEFAULT, size);
+  scint->sendMessage(SCI_STYLECLEARALL, 0, 0);
+  scint->sendMessage(SCI_STYLESETBOLD, SCE_C_WORD, true);
+  scint->sendMessage(SCI_STYLESETBOLD, SCE_C_OPERATOR, true);
+  scint->sendMessage(SCI_STYLESETITALIC, SCE_C_COMMENT, true);
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_DEFAULT, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_COMMENT, rgb("#ff00ff"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_COMMENTLINE, rgb("#ff00ff"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_COMMENTDOC, rgb("#ff00ff"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_NUMBER, rgb("#0000ff"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_WORD, rgb("#a06000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_STRING, rgb("#00dd00"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_CHARACTER, rgb("#00dd00"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_UUID, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_PREPROCESSOR, rgb("#6060e0"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_OPERATOR, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_IDENTIFIER, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_STRINGEOL, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_VERBATIM, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_REGEX, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_COMMENTLINEDOC, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_WORD2, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_COMMENTDOCKEYWORD, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_COMMENTDOCKEYWORDERROR, rgb("#000000"));
+  scint->sendMessage(SCI_STYLESETFORE, SCE_C_GLOBALCLASS, rgb("#000000"));
+}
+
+
+long Test::onCmdFont(FXObject *obj, FXSelector sel, void *ptr)
+{
+  FXFontDialog diag(this, "Toto", 0);
+  if (diag.execute()) {
+#ifdef FOX_1_6
+    FXFontDesc desc;
+    diag.getFontSelection(desc);
+#else
+    FXFontDesc desc=diag.getFontDesc();
+#endif
+    setFont(desc.face, desc.size / 10);
+  }
+  return 1;
+}
+
+
+long Test::onCmdScintilla(FXObject *obj, FXSelector sel, void *ptr)
+{
+  SCNotification * scn = static_cast<SCNotification *>(ptr);
+  // Add stuff here to handle Scintilla events
+  return 1;
+}
+
 
 FXDEFMAP(Test) TestMap[]={
   FXMAPFUNC(SEL_COMMAND, Test::ID_SCINTILLA, Test::onCmdScintilla),
@@ -151,13 +134,13 @@ FXDEFMAP(Test) TestMap[]={
 FXIMPLEMENT(Test,FXMainWindow,TestMap,ARRAYNUMBER(TestMap))
 
 
-int main(int argc, char ** argv)
+int main(int argc, char *argv[])
 {
-	// Fox stuff
 	FXApp application(FXString("test"));
 	application.init(argc, argv);
 	Test * win = new Test(&application);
 	application.create();
+  win->setTitle(FXString("FXScintilla-")+FXScintilla::version());
 	win->show(PLACEMENT_SCREEN);
 	application.run();
 	return 0;
